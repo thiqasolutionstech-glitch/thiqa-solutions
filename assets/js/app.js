6fc1b2f243615f163b8ba29b247d84ee3963a7a1
Thiqa Solutions <thiqasolutions.tech@gmail.com>
2026-06-28 18:19:00 +0300

    Describe your changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -84,3 +84,29 @@ if (contactForm) {
     });
 
 }
+
+
+
+
+
+
+
+
+const menuToggle = document.getElementById("menuToggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
